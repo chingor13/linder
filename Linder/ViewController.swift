963bdfef448f067e45d85643c6a7bda66c369ca3
Jeff Ching <ching.jeff@gmail.com>
2014-10-14 19:21:37 -0700

    Initial Commit

---
+++ Linder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Linder
+//
+//  Created by Jeffrey Ching on 10/14/14.
+//  Copyright (c) 2014 Jeffrey Ching. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
