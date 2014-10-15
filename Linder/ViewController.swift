a9edbcd368b7a35a772f830cd960a3422b221e3b
Jeff Ching <ching.jeff@gmail.com>
2014-10-14 19:45:41 -0700

    add swipe gestures and some story boards

--- Linder/ViewController.swift
+++ Linder/ViewController.swift
@@ -8,8 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var noButton: UIButton!
+    @IBOutlet weak var yesButton: UIButton!
+
+    @IBAction func swipeLeft(sender: AnyObject) {
+        println("swipe left")
+    }
+    @IBAction func swipeRight(sender: AnyObject) {
+        println("swipe right")
+    }
+    
+    @IBAction func yesClick(sender: AnyObject) {
+        println("yes click");
+    }
+    
+    func gestureRecognizer(recognizer: UIGestureRecognizer, shouldReceiveTouch touch: UITouch) {
+        println("recevied swipe")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
